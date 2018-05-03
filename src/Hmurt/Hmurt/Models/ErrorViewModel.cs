96b49aeb73d11c5e1e4e2de2ec8dff5da4526113
Henri Murtonen <henri.murtonen@gmail.com>
2018-05-03 22:31:01 +0300

    Changed project to mvc

---
+++ src/Hmurt/Hmurt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hmurt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
