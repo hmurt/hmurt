fab4f52879de23eb405e76a901903d35186a2321
Henri Murtonen <henri.murtonen@gmail.com>
2018-05-19 11:10:15 +0300

    Static port mapping for k8s

--- src/Hmurt/Hmurt/Program.cs
+++ src/Hmurt/Hmurt/Program.cs
@@ -19,6 +19,7 @@ namespace Hmurt
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:8080")
                 .UseStartup<Startup>()
                 .Build();
     }
